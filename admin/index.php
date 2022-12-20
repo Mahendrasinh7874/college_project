28c10df8fc362248b0a7f87383ea77eee1968342
unknown <mahendrazala945@gmail.com>
2022-12-20 19:01:28 +0530

    ..

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,33 @@
 
+ <?php
+        if (isset($_POST['submit'])) {
+          include 'config.php';
+          $email = $_POST['email'];
+          $password = $_POST['password'];
+
+          $sql = "Select * from admin where email='$email' AND password='$password'";
+
+
+          $result = mysqli_query($conn, $sql) or die('Query Failed');
+
+          // $num =mysqli_num_rows($result);
+        
+          if (mysqli_num_rows($result) == 1) {
+            while ($row = mysqli_fetch_assoc($result)) {
+              session_start();
+              $_SESSION['email'] = $row['email'];
+              // $_SESSION['password'] = $row['password'];
+        
+              // header('Location : dashboard.php');
+            
+              header('Location: '.$newURL);
+
+            }
+          } else {
+            echo '<p style="font-size:15px;color:red;">Please Enter Valid Email and Password!</p>';
+          }
+        }
+        ?>
 
 
 
@@ -6,36 +35,34 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/login.css" />
-    
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/login.css" />
+
+
+  <title>Login</title>
 </head>
 
 <body>
-    <div class="login-container">
-        <div class="login-box">
-            <form action="admin_panel.php" method="POST" class="form-input">
-                <p>Admin Login</p>
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" class="input" required placeholder="Email" />
-                <label for="password">Password</label>
-                <input type="password" required name="password" class="input" id="password" placeholder="Password" />
-                <!-- <button type="submit">Login</button> -->
-                <input type="submit" id="btn" name="btn" value="Login" />
+  <div class="login-container">
+    <div class="login-box">
 
-                <?php
-                // if (isset($_SESSION['admin_log'])) {
-                //     echo "<h4 class='text-danger'>please enter valid email and password.";
-                //     session_unset();
-                //     session_destroy();
-                // }
-                ?>
-            </form>
-        </div>
+      <!-- <form action=?>" method="POST" class="form-input"> -->
+      <form method="post" class="form-input" action="dashboard.php">
+
+        <p>Admin Login</p>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" class="input" required placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" required name="password" class="input" id="password" placeholder="Password" />
+        <!-- <button type="submit">Login</button> -->
+        <input type="submit" id="btn" name="btn" value="Login" />
+
+       
+      </form>
     </div>
+  </div>
 </body>
 
 </html>
